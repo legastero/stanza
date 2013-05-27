97c2f289252eb8250054898a7b13314cac7376d1
Lance Stout <lancestout@gmail.com>
2013-05-26 23:28:06 -0700

    Add delayed delivery, organize

--- index.js
+++ index.js
@@ -3,19 +3,6 @@ exports.Stream = require('./lib/stanza/stream').Stream;
 exports.Message = require('./lib/stanza/message').Message;
 exports.Presence = require('./lib/stanza/presence').Presence;
 exports.Iq = require('./lib/stanza/iq').Iq;
-exports.Error = require('./lib/stanza/error').Error;
-exports.Roster = require('./lib/stanza/roster').Roster;
-
-exports.StreamError = require('./lib/stanza/streamError').StreamError;
-exports.StreamFeatures = require('./lib/stanza/streamFeatures').StreamFeatures;
-exports.Bind = require('./lib/stanza/bind').Bind;
-exports.Session = require('./lib/stanza/session').Session;
-exports.SASL = require('./lib/stanza/sasl');
-exports.TLS = require('./lib/stanza/starttls');
-
-exports.SM = require('./lib/stanza/sm');
-
-exports.ChatState = require('./lib/stanza/chatState.js');
 
 exports.SOX = require('./lib/stanza/sox');
 
@@ -30,6 +17,7 @@ exports.createClient = function (opts) {
 
     client.use(require('./lib/plugins/disco').init);
     client.use(require('./lib/plugins/chatstates').init);
+    client.use(require('./lib/plugins/delayed').init);
     client.use(require('./lib/plugins/webrtc').init);
 
     return client;
