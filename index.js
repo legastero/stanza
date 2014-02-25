15b158a9c17563119c0bc0595764ff87d9950a59
Lance Stout <lancestout@gmail.com>
2014-02-25 09:57:20 -0800

    Add some extra utility exports for using JXT when bundled

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ exports.Message = require('./lib/stanza/message');
 exports.Presence = require('./lib/stanza/presence');
 exports.Iq = require('./lib/stanza/iq');
 
+exports.PubsubEvent = require('./lib/stanza/pubsub').EventItem;
+exports.PubsubItem = require('./lib/stanza/pubsub').Item;
+
 exports.JID = require('./lib/jid');
 
 exports.Client = require('./lib/client');
 exports.crypto = require('crypto');
+exports.jxt = require('jxt');
+
 
 exports.createClient = function (opts) {
     var client = new exports.Client(opts);
