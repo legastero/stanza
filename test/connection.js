88f2ade15a378f4f3a388afbf74d7d65bbf6e3b2
Lance Stout <lancestout@gmail.com>
2014-03-27 02:01:48 -0700

    Start test suite

---
+++ test/connection.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var test = require('tape');
+var stanza = require('../index');
+
+
+test('Connect using WebSocket', function (t) {
+    t.plan(1);
+
+    var client = stanza.createClient({
+        jid: 'anon@anon.lance.im',
+        transport: 'websocket',
+        wsURL: 'wss://lance.im:5281/xmpp-websocket'
+    });
+
+    client.on('session:started', function () {
+        t.pass('Connected with WebSocket');
+        client.disconnect();
+    });
+
+    client.connect();
+});
+
+test('Connect using BOSH', function (t) {
+    t.plan(1);
+
+    var client = stanza.createClient({
+        jid: 'anon@anon.lance.im',
+        transport: 'bosh',
+        boshURL: 'https://lance.im:5281/http-bind'
+    });
+
+    client.on('session:started', function () {
+        t.pass('Connected with BOSH');
+        client.disconnect();
+    });
+
+    client.connect();
+});
