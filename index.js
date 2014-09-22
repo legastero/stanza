8fcacf9be5eb092188bf65072a8ccc24d2a50892
Lance Stout <lancestout@gmail.com>
2014-09-22 10:39:12 -0700

    These top-level no longer apply

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 'use strict';
 
-exports.lang = process.env.LANG;
-
-exports.Stream = require('./lib/stanza/stream');
-exports.BOSH = require('./lib/stanza/bosh');
-exports.Message = require('./lib/stanza/message');
-exports.Presence = require('./lib/stanza/presence');
-exports.Iq = require('./lib/stanza/iq');
-
-exports.PubsubEvent = require('./lib/stanza/pubsubEvents').EventItem;
-exports.PubsubItem = require('./lib/stanza/pubsub').Item;
-
 exports.JID = require('xmpp-jid').JID;
-
 exports.Client = require('./lib/client');
-exports.crypto = require('crypto');
-exports.jxt = require('jxt');
-
-exports.jingle = require('./lib/stanza/jingle');
 
 
 exports.createClient = function (opts) {
