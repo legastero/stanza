fa61902f16180aa1229be166a4bb702740afb6fa
Lance Stout <lancestout@gmail.com>
2018-11-05 11:32:50 -0800

    Linting: 4-space indent

--- webpack.config.js
+++ webpack.config.js
@@ -5,34 +5,34 @@ const Pkg = require('./package.json');
 
 
 module.exports = {
-  entry: './index.js',
+    entry: './index.js',
 
-  output: {
-    filename: 'stanzaio.bundle.js',
-    library: 'XMPP',
-    libraryTarget: 'window',
-    path: path.resolve('build')
-  },
+    output: {
+        filename: 'stanzaio.bundle.js',
+        library: 'XMPP',
+        libraryTarget: 'window',
+        path: path.resolve('build')
+    },
 
-  module: {
-    rules: [
-      {
-        test: /index\.js/,
-        loader: 'string-replace-loader',
-        options: {
-          search: '__STANZAIO_VERSION__',
-          replace: Pkg.version
-        }
-      }
+    module: {
+        rules: [
+            {
+                test: /index\.js/,
+                loader: 'string-replace-loader',
+                options: {
+                    search: '__STANZAIO_VERSION__',
+                    replace: Pkg.version
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            reportFilename: 'bundle-stats.html',
+            defaultSizes: 'gzip'
+        })
     ]
-  },
-
-  plugins: [
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      reportFilename: 'bundle-stats.html',
-      defaultSizes: 'gzip'
-    })
-  ]
 };
 
