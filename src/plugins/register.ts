d7c797cd52254ee9933f4777011a1a7eb490c6be
Lance Stout <lancestout@gmail.com>
2019-07-01 17:20:32 -0700

    Tighten account management IQ results

--- src/plugins/register.ts
+++ src/plugins/register.ts
@@ -3,19 +3,20 @@ import { AccountManagement, IQ } from '../protocol';
 
 declare module '../' {
     export interface Agent {
-        getAccountInfo(jid?: string): Promise<IQ>;
+        getAccountInfo(jid?: string): Promise<AccountManagement>;
         updateAccount(jid: string, data: AccountManagement): Promise<IQ>;
         deleteAccount(jid: string): Promise<IQ>;
     }
 }
 
 export default function(client: Agent) {
-    client.getAccountInfo = (jid?: string) => {
-        return client.sendIQ({
-            account: {},
+    client.getAccountInfo = async (jid?: string) => {
+        const resp = await client.sendIQ({
+            account: {} as AccountManagement,
             to: jid,
             type: 'get'
         });
+        return resp.account;
     };
 
     client.updateAccount = (jid: string | undefined, data: AccountManagement) => {
