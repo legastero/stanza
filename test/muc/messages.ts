3e592ddf9b517948decb309fc691959c0d0fd438
Lance Stout <lancestout@gmail.com>
2019-12-22 21:35:56 -0800

    Add MUC event tests

---
+++ test/muc/messages.ts
@@ -0,0 +1,187 @@
+import { createClient } from '../../src';
+import { ReceivedMessage } from '../../src/protocol';
+
+test('MUC chat', () => {
+    const client = createClient({});
+
+    const incoming: ReceivedMessage = {
+        body: 'yayyyy',
+        from: 'room@rooms.test/member',
+        to: 'tester@localhost',
+        type: 'groupchat'
+    };
+
+    client.on('groupchat', msg => {
+        expect(msg).toStrictEqual(incoming);
+    });
+
+    client.emit('message', incoming);
+});
+
+test('MUC subject', () => {
+    const client = createClient({});
+
+    const incoming: ReceivedMessage = {
+        hasSubject: true,
+        from: 'room@rooms.test/admin',
+        to: 'tester@localhost',
+        subject: 'Room subject',
+        type: 'groupchat'
+    };
+
+    client.on('muc:topic', msg => {
+        expect(msg).toStrictEqual({
+            from: 'room@rooms.test/admin',
+            room: 'room@rooms.test',
+            topic: 'Room subject'
+        });
+    });
+
+    client.emit('message', incoming);
+});
+
+test('MUC empty subject', () => {
+    const client = createClient({});
+
+    const incoming: ReceivedMessage = {
+        hasSubject: true,
+        from: 'room@rooms.test/admin',
+        to: 'tester@localhost',
+        subject: '',
+        type: 'groupchat'
+    };
+
+    client.on('muc:topic', msg => {
+        expect(msg).toStrictEqual({
+            from: 'room@rooms.test/admin',
+            room: 'room@rooms.test',
+            topic: ''
+        });
+    });
+
+    client.emit('message', incoming);
+});
+
+test('MUC mediated invite', () => {
+    const client = createClient({});
+
+    const incoming: ReceivedMessage = {
+        from: 'room@rooms.test',
+        muc: {
+            type: 'info',
+            invite: [
+                {
+                    from: 'room@rooms.test/member',
+                    reason: 'because',
+                    thread: '1-1-1'
+                }
+            ],
+            password: 'secret'
+        },
+        to: 'tester@localhost',
+        type: 'groupchat'
+    };
+
+    client.on('muc:invite', msg => {
+        expect(msg).toStrictEqual({
+            from: 'room@rooms.test/member',
+            password: 'secret',
+            reason: 'because',
+            room: 'room@rooms.test',
+            thread: '1-1-1',
+            type: 'mediated'
+        });
+    });
+
+    client.emit('message', incoming);
+});
+
+test('MUC direct invite', () => {
+    const client = createClient({});
+
+    const incoming: ReceivedMessage = {
+        from: 'peer@localhost',
+        muc: {
+            type: 'direct-invite',
+            jid: 'room@rooms.test',
+            password: 'secret',
+            reason: 'because',
+            thread: '1-1-1'
+        },
+        to: 'tester@localhost',
+        type: 'groupchat'
+    };
+
+    client.on('muc:invite', msg => {
+        expect(msg).toStrictEqual({
+            from: 'peer@localhost',
+            password: 'secret',
+            reason: 'because',
+            room: 'room@rooms.test',
+            thread: '1-1-1',
+            type: 'direct'
+        });
+    });
+
+    client.emit('message', incoming);
+});
+
+test('MUC PM and direct invite', () => {
+    const client = createClient({});
+
+    const incoming: ReceivedMessage = {
+        from: 'peer@localhost',
+        legacyMUC: {
+            type: 'direct-invite',
+            jid: 'room@rooms.test',
+            password: 'secret',
+            reason: 'because',
+            thread: '1-1-1'
+        },
+        muc: {
+            type: 'info'
+        },
+        to: 'tester@localhost',
+        type: 'groupchat'
+    };
+
+    client.on('muc:invite', msg => {
+        expect(msg).toStrictEqual({
+            from: 'peer@localhost',
+            password: 'secret',
+            reason: 'because',
+            room: 'room@rooms.test',
+            thread: '1-1-1',
+            type: 'direct'
+        });
+    });
+
+    client.emit('message', incoming);
+});
+
+test('MUC declined invite', () => {
+    const client = createClient({});
+
+    const incoming: ReceivedMessage = {
+        from: 'room@rooms.test',
+        to: 'tester@localhost',
+        type: 'groupchat',
+        muc: {
+            decline: {
+                from: 'invited@localhost',
+                reason: 'busy'
+            },
+            type: 'info'
+        }
+    };
+
+    client.on('muc:declined', msg => {
+        expect(msg).toStrictEqual({
+            from: 'invited@localhost',
+            room: 'room@rooms.test',
+            reason: 'busy'
+        });
+    });
+
+    client.emit('message', incoming);
+});
---
+++ test/muc/presence.ts
@@ -0,0 +1,153 @@
+import { createClient } from '../../src';
+import { ReceivedPresence } from '../../src/protocol';
+
+test('MUC available', () => {
+    const client = createClient({});
+
+    const incoming: ReceivedPresence = {
+        from: 'room@rooms.test/peer',
+        muc: {
+            type: 'info'
+        },
+        to: 'tester@localhost'
+    };
+
+    client.on('muc:available', msg => {
+        expect(msg).toStrictEqual(incoming);
+    });
+
+    client.emit('presence', incoming);
+});
+
+test('MUC unavailable', () => {
+    const client = createClient({});
+
+    const incoming: ReceivedPresence = {
+        from: 'room@rooms.test/peer',
+        muc: {
+            type: 'info'
+        },
+        to: 'tester@localhost',
+        type: 'unavailable'
+    };
+
+    client.on('muc:unavailable', msg => {
+        expect(msg).toStrictEqual(incoming);
+    });
+
+    client.emit('presence', incoming);
+});
+
+test('MUC join', () => {
+    const client = createClient({});
+
+    const incoming: ReceivedPresence = {
+        from: 'room@rooms.test/self',
+        muc: {
+            type: 'info',
+            statusCodes: ['110']
+        },
+        to: 'tester@localhost'
+    };
+
+    client.on('muc:join', msg => {
+        expect(msg).toStrictEqual(incoming);
+    });
+
+    client.emit('presence', incoming);
+});
+
+test('MUC join failed', () => {
+    const client = createClient({});
+    client.joiningRooms.set('room@rooms.test', 'self');
+
+    const incoming: ReceivedPresence = {
+        from: 'room@rooms.test/self',
+        type: 'error',
+        to: 'tester@localhost'
+    };
+
+    client.on('muc:failed', msg => {
+        expect(msg).toStrictEqual(incoming);
+    });
+    client.on('muc:error', msg => {
+        expect(msg).toStrictEqual(incoming);
+    });
+
+    client.emit('presence', incoming);
+});
+
+test('MUC error', () => {
+    const client = createClient({});
+    client.joinedRooms.set('room@rooms.test', 'self');
+
+    const incoming: ReceivedPresence = {
+        from: 'room@rooms.test/self',
+        type: 'error',
+        to: 'tester@localhost',
+        muc: {
+            type: 'info'
+        }
+    };
+
+    client.on('muc:error', msg => {
+        expect(msg).toStrictEqual(incoming);
+    });
+
+    client.emit('presence', incoming);
+});
+
+test('MUC leave', () => {
+    const client = createClient({});
+    client.joinedRooms.set('room@rooms.test', 'self');
+
+    const incoming: ReceivedPresence = {
+        from: 'room@rooms.test/self',
+        type: 'unavailable',
+        to: 'tester@localhost',
+        muc: {
+            type: 'info',
+            statusCodes: ['110']
+        }
+    };
+
+    client.on('muc:leave', msg => {
+        expect(msg).toStrictEqual(incoming);
+    });
+
+    client.emit('presence', incoming);
+});
+
+test('MUC destroyed', () => {
+    const client = createClient({});
+    client.joinedRooms.set('room@rooms.test', 'self');
+
+    const incoming: ReceivedPresence = {
+        from: 'room@rooms.test/self',
+        type: 'unavailable',
+        to: 'tester@localhost',
+        muc: {
+            type: 'info',
+            statusCodes: ['110'],
+            destroy: {
+                reason: 'done here',
+                jid: 'new-room@rooms.test',
+                password: 'secret'
+            }
+        }
+    };
+
+    client.on('muc:leave', msg => {
+        expect(msg).toStrictEqual(incoming);
+    });
+    client.on('muc:destroyed', msg => {
+        expect(msg).toStrictEqual({
+            newRoom: 'new-room@rooms.test',
+            password: 'secret',
+            reason: 'done here',
+            room: 'room@rooms.test'
+        });
+    });
+
+    client.emit('presence', incoming);
+});
