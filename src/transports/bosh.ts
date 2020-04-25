714331b990bde119c41d4649692f26bf188ab10b
Lance Stout <lancestout@gmail.com>
2020-04-25 10:32:20 -0700

    Make BOSH transport based on duplex stream

--- src/transports/bosh.ts
+++ src/transports/bosh.ts
@@ -1,100 +1,217 @@
+import { Duplex } from 'readable-stream';
 import { fetch } from 'stanza-shims';
 
 import { Agent, Transport, TransportConfig } from '../';
 import { StreamErrorCondition } from '../Constants';
 import StreamManagement from '../helpers/StreamManagement';
+import { Stream } from '../protocol';
+
 import { ParsedData, Registry, StreamParser } from '../jxt';
-import { BOSH, Stream } from '../protocol';
+
 import { sleep, timeoutPromise } from '../Utils';
 
-async function retryRequest(
-    url: string,
-    opts: RequestInit,
-    timeout: number,
-    allowedRetries: number = 5
-): Promise<string> {
-    let attempt = 0;
-    while (attempt <= allowedRetries) {
-        try {
-            const resp = await timeoutPromise(fetch(url, opts), timeout * 1000, () => {
-                return new Error('Request timed out');
-            });
-            if (!resp.ok) {
-                throw new Error('HTTP Status Error: ' + resp.status);
-            }
-            return resp.text();
-        } catch (err) {
-            attempt += 1;
-            if (attempt > allowedRetries) {
-                throw err;
+class RequestChannel {
+    public rid!: number;
+    public maxTimeout: number;
+    public active: boolean = false;
+
+    private stream: BOSH;
+    private maxRetries: number = 5;
+
+    constructor(stream: BOSH) {
+        this.stream = stream;
+        this.maxTimeout = 1000 * 1.1 * this.stream.maxWaitTime;
+    }
+
+    public async send(rid: number, body: string): Promise<string> {
+        this.rid = rid;
+        this.active = true;
+        let attempts = 0;
+
+        while (attempts <= this.maxRetries) {
+            attempts += 1;
+
+            try {
+                const res = await timeoutPromise(
+                    fetch(this.stream.url, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': this.stream.contentType
+                        },
+                        body
+                    }),
+                    this.maxTimeout,
+                    () => new Error('Request timed out')
+                );
+                if (!res.ok) {
+                    throw new Error('HTTP Status Error: ' + res.status);
+                }
+
+                const result = await res.text();
+                this.active = false;
+
+                return result;
+            } catch (err) {
+                if (attempts === 1) {
+                    continue;
+                } else if (attempts < this.maxRetries) {
+                    const backoff = Math.min(this.maxTimeout, Math.pow(attempts, 2) * 1000);
+                    await sleep(backoff + Math.random() * 1000);
+                    continue;
+                } else {
+                    this.active = false;
+                    throw err;
+                }
             }
         }
 
-        await sleep(Math.pow(attempt, 2) * 1000);
+        throw new Error('Request failed');
     }
-
-    throw new Error('Request failed');
 }
 
-export default class BOSHConnection implements Transport {
+export default class BOSH extends Duplex implements Transport {
     public hasStream?: boolean;
     public stream?: Stream;
     public authenticated?: boolean;
 
-    public sid?: string;
-    public rid?: number;
-
     private client: Agent;
     private config!: TransportConfig;
-    private url?: string;
     private sm: StreamManagement;
     private stanzas: Registry;
-    private closing?: boolean;
-    private sendBuffer: string[];
-    private requests: Set<number>;
 
-    private maxRequests: number = 2;
-    private maxHoldOpen: number = 1;
-    private minPollingInterval: number = 5;
+    public url!: string;
+    public rid?: number = Math.floor(Math.random() * 0xffffffff);
+    public sid?: string = '';
 
-    private pollingInterval: any;
+    public maxHoldOpen: number = 2;
+    public maxWaitTime: number = 30;
+
+    public contentType: string = 'text/xml; charset=utf-8';
+
     private idleTimeout: any;
-    private lastResponseTime: number;
+    private queue: Array<[any, (err?: Error) => void]> = [];
+
+    private isEnded: boolean = false;
+
+    public channels: RequestChannel[] = [new RequestChannel(this), new RequestChannel(this)];
+    public activeChannelID = 0;
 
     constructor(client: Agent, sm: StreamManagement, stanzas: Registry) {
+        super({
+            objectMode: true
+        });
+
         this.client = client;
         this.sm = sm;
         this.stanzas = stanzas;
-        this.sendBuffer = [];
-        this.requests = new Set();
-        this.authenticated = false;
-        this.lastResponseTime = Date.now();
 
-        this.pollingInterval = setInterval(() => {
-            if (
-                this.authenticated &&
-                this.requests.size === 0 &&
-                this.sendBuffer.length === 0 &&
-                Date.now() - this.lastResponseTime >= this.minPollingInterval * 1000
-            ) {
-                this.longPoll();
-            }
-        }, 1000);
+        this.on('data', e => {
+            this.client.emit('stream:data', e.stanza, e.kind);
+        });
+
+        this.on('end', () => {
+            this.isEnded = true;
+            clearTimeout(this.idleTimeout);
+
+            this.client.emit('--transport-disconnected');
+        });
     }
 
-    public connect(opts: TransportConfig) {
-        this.config = {
-            maxRetries: 5,
-            rid: Math.ceil(Math.random() * 9999999999),
-            wait: 30,
-            ...opts
-        };
-        this.hasStream = false;
-        this.sm.started = false;
-        this.url = this.config.url;
-        this.sid = this.config.sid;
-        this.rid = this.config.rid;
-        this.requests.clear();
+    public _write(chunk: any, encoding: string, done: (err?: Error) => void) {
+        this.queue.push([chunk, done]);
+        this.scheduleRequests();
+    }
+
+    public _writev(chunks: Array<{ chunk: any; encoding: any }>, done: (err?: Error) => void) {
+        this.queue.push([chunks.map(c => c.chunk).join(''), done]);
+        this.scheduleRequests();
+    }
+
+    public _read(): void {
+        return;
+    }
+
+    private get sendingChannel() {
+        return this.channels[this.activeChannelID];
+    }
+
+    private get pollingChannel() {
+        return this.channels[this.activeChannelID === 0 ? 1 : 0];
+    }
+
+    private toggleChannel() {
+        this.activeChannelID = this.activeChannelID === 0 ? 1 : 0;
+    }
+
+    public process(result: string): void {
+        const parser = new StreamParser({
+            acceptLanguages: this.config.acceptLanguages,
+            allowComments: false,
+            lang: this.config.lang,
+            registry: this.stanzas,
+            rootKey: 'bosh',
+            wrappedStream: true
+        });
+
+        parser.on('error', (err: any) => {
+            const streamError = {
+                condition: StreamErrorCondition.InvalidXML
+            };
+            this.client.emit('stream:error', streamError, err);
+            this.send('error', streamError);
+            return this.disconnect();
+        });
+
+        parser.on('data', (e: ParsedData) => {
+            if (e.event === 'stream-start') {
+                this.stream = e.stanza;
+                if (e.stanza.type === 'terminate') {
+                    this.hasStream = false;
+                    this.rid = undefined;
+                    this.sid = undefined;
+                    if (!this.isEnded) {
+                        this.isEnded = true;
+                        this.client.emit('bosh:terminate', e.stanza);
+                        this.client.emit('stream:end');
+                        this.end();
+                    }
+                } else if (!this.hasStream) {
+                    this.hasStream = true;
+                    this.stream = e.stanza;
+                    this.sid = e.stanza.sid || this.sid;
+                    this.maxWaitTime = e.stanza.maxWaitTime || this.maxWaitTime;
+
+                    this.client.emit('stream:start', e.stanza);
+                }
+
+                return;
+            }
+
+            if (!e.event) {
+                this.push({ kind: e.kind, stanza: e.stanza });
+            }
+        });
+
+        this.client.emit('raw', 'incoming', result);
+        parser.write(result);
+
+        this.scheduleRequests();
+    }
+
+    public connect(opts: TransportConfig): void {
+        this.config = opts;
+
+        this.url = opts.url;
+        if (opts.rid) {
+            this.rid = opts.rid;
+        }
+        if (opts.sid) {
+            this.sid = opts.sid;
+        }
+        if (opts.wait) {
+            this.maxWaitTime = opts.wait;
+        }
+
         if (this.sid) {
             this.hasStream = true;
             this.stream = {};
@@ -103,22 +220,28 @@ export default class BOSHConnection implements Transport {
             this.client.emit('session:started');
             return;
         }
-        this.rid!++;
-        this.request({
-            lang: this.config.lang,
-            maxHoldOpen: 1,
-            maxWaitTime: this.config.wait,
-            to: this.config.server,
+
+        this._send({
+            to: opts.server,
+            lang: opts.lang,
             version: '1.6',
+            maxWaitTime: this.maxWaitTime,
+            maxHoldOpen: this.maxHoldOpen,
             xmppVersion: '1.0'
         });
     }
 
+    public restart(): void {
+        this.hasStream = false;
+        this._send({
+            to: this.config.server,
+            xmppRestart: true
+        });
+    }
+
     public disconnect() {
-        clearInterval(this.pollingInterval);
         if (this.hasStream) {
-            this.rid!++;
-            this.request({
+            this._send({
                 type: 'terminate'
             });
         } else {
@@ -129,155 +252,98 @@ export default class BOSHConnection implements Transport {
         }
     }
 
-    public restart() {
-        this.hasStream = false;
-        this.rid!++;
-        this.request({
-            lang: this.config.lang,
-            to: this.config.server,
-            xmppRestart: true
-        });
-    }
-
     public send(dataOrName: string, data?: object): void {
         if (data) {
             const output = this.stanzas.export(dataOrName, data);
             if (output) {
-                this.sendBuffer.push(output.toString());
+                this.write(output.toString());
             }
         } else {
-            this.sendBuffer.push(dataOrName);
+            this.write(dataOrName);
         }
-        this.longPoll();
     }
 
-    private longPoll() {
-        const canReceive = !this.maxRequests || this.requests.size < this.maxRequests;
-        const canSend =
-            !this.maxRequests ||
-            (this.sendBuffer.length > 0 && this.requests.size < this.maxRequests);
-
-        if (!this.sid || (!canReceive && !canSend)) {
+    private async _send(boshData: any, payload: string = ''): Promise<void> {
+        if (this.isEnded) {
             return;
         }
 
-        const stanzas = this.sendBuffer;
-        this.sendBuffer = [];
-        this.rid!++;
-        this.request({}, stanzas);
-    }
-
-    private async request(meta: BOSH, payloads: string[] = []) {
-        const rid = this.rid!;
-        this.requests.add(rid);
-
-        meta.rid = this.rid;
-        meta.sid = this.sid;
-
-        const bosh = this.stanzas.export('bosh', meta)!;
-        const body = [bosh.openTag(), ...payloads, bosh.closeTag()].join('');
+        const rid = this.rid!++;
+        const header = this.stanzas.export('bosh', {
+            ...boshData,
+            rid,
+            sid: this.sid
+        })!;
+        let body: string;
+        if (payload) {
+            body = [header.openTag(), payload, header.closeTag()].join('');
+        } else {
+            body = header.toString();
+        }
 
         this.client.emit('raw', 'outgoing', body);
 
-        try {
-            const respBody = await retryRequest(
-                this.url!,
-                {
-                    body,
-                    headers: {
-                        'Content-Type': 'text/xml'
-                    },
-                    method: 'POST'
-                },
-                this.config.wait! * 1.1,
-                this.config.maxRetries
-            );
-            this.requests.delete(rid);
-            this.lastResponseTime = Date.now();
+        this.sendingChannel
+            .send(rid, body)
+            .then(result => {
+                this.process(result);
+            })
+            .catch(err => {
+                this.end(err);
+            });
+        this.toggleChannel();
+    }
 
-            if (respBody) {
-                const rawData = Buffer.from(respBody, 'utf8').toString().trim();
-                if (rawData === '') {
-                    return;
-                }
-                this.client.emit('raw', 'incoming', rawData);
+    private async _poll(): Promise<void> {
+        if (this.isEnded) {
+            return;
+        }
 
-                const parser = new StreamParser({
-                    acceptLanguages: this.config.acceptLanguages,
-                    allowComments: false,
-                    lang: this.config.lang,
-                    registry: this.stanzas,
-                    rootKey: 'bosh',
-                    wrappedStream: true
-                });
+        const rid = this.rid!++;
+        const body = this.stanzas
+            .export('bosh', {
+                rid,
+                sid: this.sid
+            })!
+            .toString();
 
-                parser.on('error', (err: any) => {
-                    const streamError = {
-                        condition: StreamErrorCondition.InvalidXML
-                    };
-                    this.client.emit('stream:error', streamError, err);
-                    this.send('error', streamError);
-                    return this.disconnect();
-                });
+        this.client.emit('raw', 'outgoing', body);
 
-                parser.on('data', (e: ParsedData) => {
-                    if (e.event === 'stream-start') {
-                        if (e.stanza.type === 'terminate') {
-                            this.hasStream = false;
-                            this.rid = undefined;
-                            this.sid = undefined;
-                            this.client.emit('bosh:terminate', e.stanza);
-                            this.client.emit('stream:end');
-                            this.client.emit('--transport-disconnected');
-                            return;
-                        }
-                        if (!this.hasStream) {
-                            this.hasStream = true;
-                            this.stream = e.stanza;
-                            this.sid = e.stanza.sid || this.sid;
-                            this.maxHoldOpen = e.stanza.maxHoldOpen || this.maxHoldOpen;
-                            this.maxRequests = e.stanza.maxRequests || this.maxRequests;
-                            this.minPollingInterval =
-                                e.stanza.minPollingInterval || this.minPollingInterval;
+        this.pollingChannel
+            .send(rid, body)
+            .then(result => {
+                this.process(result);
+            })
+            .catch(err => {
+                this.end(err);
+            });
+    }
 
-                            this.client.emit('stream:start', e.stanza);
-                        }
-                        return;
-                    }
-                    if (!e.event) {
-                        this.client.emit('stream:data', e.stanza, e.kind);
-                    }
-                });
+    private scheduleRequests() {
+        clearTimeout(this.idleTimeout);
+        this.idleTimeout = setTimeout(() => {
+            this.fireRequests();
+        }, 10);
+    }
 
-                parser.write(rawData);
+    private fireRequests() {
+        if (this.isEnded) {
+            return;
+        }
+
+        if (this.queue.length) {
+            if (!this.sendingChannel.active) {
+                const [data, done] = this.queue.shift()!;
+                this._send({}, data);
+                done();
+            } else {
+                this.scheduleRequests();
             }
+            return;
+        }
 
-            if (meta.type === 'terminate') {
-                this.closing = true;
-            }
-
-            // do not (re)start long polling if terminating, or request is pending, or before authentication
-            if (
-                this.hasStream &&
-                !this.closing &&
-                this.authenticated &&
-                (!this.requests.size || this.sendBuffer.length)
-            ) {
-                clearTimeout(this.idleTimeout);
-                this.idleTimeout = setTimeout(() => {
-                    this.longPoll();
-                }, 100);
-            }
-        } catch (err) {
-            this.hasStream = false;
-            this.client.emit(
-                'stream:error',
-                {
-                    condition: 'connection-timeout'
-                },
-                err
-            );
-            this.disconnect();
+        if (this.authenticated && !(this.channels[0].active || this.channels[1].active)) {
+            this._poll();
         }
     }
 }
