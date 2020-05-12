a8d4670386adaf2a964cecbc735cb77c971b7c71
Lance Stout <lancestout@gmail.com>
2020-05-12 11:33:48 -0700

    Expand mapped content for Atom entries

--- src/protocol/rfc4287.ts
+++ src/protocol/rfc4287.ts
@@ -18,28 +18,40 @@ import { PubsubItemContent } from './';
 
 export interface AtomEntry extends PubsubItemContent {
     itemType?: typeof NS_ATOM;
-    title?: string;
-    summary?: AtomSummary;
+    authors?: AtomPerson[];
+    categories?: AtomCategory[];
+    content?: AtomText;
+    contributors?: AtomPerson[];
     id?: string;
-    published?: Date;
-    updated?: Date;
     links?: AtomLink[];
+    published?: Date;
+    rights?: AtomText;
+    summary?: AtomText;
+    title?: AtomText;
+    updated?: Date;
 }
 
-export interface AtomTitle {
+export interface AtomText {
     text?: string;
-    type?: 'text';
-}
-
-export interface AtomSummary {
-    text?: string;
-    type?: 'text';
+    type?: 'text' | 'html';
 }
 
 export interface AtomLink {
     href?: string;
     mediaType?: string;
     rel?: string;
+}
+
+export interface AtomPerson {
+    name?: string;
+    uri?: string;
+    email?: string;
+}
+
+export interface AtomCategory {
+    term?: string;
+    scheme?: string;
+    label?: string;
 }
 
 const Protocol: DefinitionOptions[] = [
@@ -82,6 +94,54 @@ const Protocol: DefinitionOptions[] = [
             rel: attribute('rel')
         },
         namespace: NS_ATOM
+    },
+    {
+        aliases: [{ path: 'atomentry.authors', multiple: true }],
+        element: 'author',
+        fields: {
+            name: childText(null, 'name'),
+            uri: childText(null, 'uri'),
+            email: childText(null, 'email')
+        },
+        namespace: NS_ATOM
+    },
+    {
+        aliases: [{ path: 'atomentry.contributors', multiple: true }],
+        element: 'contributor',
+        fields: {
+            name: childText(null, 'name'),
+            uri: childText(null, 'uri'),
+            email: childText(null, 'email')
+        },
+        namespace: NS_ATOM
+    },
+    {
+        aliases: [{ path: 'atomentry.categories', multiple: true }],
+        element: 'category',
+        fields: {
+            term: attribute('term'),
+            scheme: attribute('scheme'),
+            label: attribute('label')
+        },
+        namespace: NS_ATOM
+    },
+    {
+        element: 'content',
+        fields: {
+            text: text(),
+            type: attribute('type', 'text')
+        },
+        namespace: NS_ATOM,
+        path: 'atomentry.content'
+    },
+    {
+        element: 'rights',
+        fields: {
+            text: text(),
+            type: attribute('type', 'text')
+        },
+        namespace: NS_ATOM,
+        path: 'atomentry.rights'
     }
 ];
 export default Protocol;
